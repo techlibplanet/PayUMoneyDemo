de4d39427d9fe77e29422114a8cd5df64ab98b4f
techlibplanet <techlibplanet@gmail.com>
2018-06-19 17:33:30 +0530

    Working upto api level 28

--- app/src/main/java/com/example/mayank/payumoneydemo/Constants.kt
+++ app/src/main/java/com/example/mayank/payumoneydemo/Constants.kt
@@ -2,8 +2,8 @@ package com.example.mayank.payumoneydemo
 
 internal object Constants {
 
-    val MERCHANT_ID = "YOUR_MERCHANT_KET"
-    val MERCHANT_KEY = "YOUR_MERCHANT_ID"
+    val MERCHANT_ID = "4873218"
+    val MERCHANT_KEY = "HqRplS"
     val surl = "https://www.payumoney.com/mobileapp/payumoney/success.php"
     val furl = "https://www.payumoney.com/mobileapp/payumoney/failure.php"
     val URL = "http://alchemyeducation.org/payu/getHashCode.php"
